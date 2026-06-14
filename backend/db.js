a34ec47fb897ccb961885107730c42482cef76c9
Claudio Oliveira <claudio.oliveira.sants@gmail.com>
2026-06-14 12:42:05 -0400

    feat: Backend criado
    conexão backend e banco de dados SQL prontos.

---
+++ backend/db.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+// conexão
+const pool = mysql.createPool({
+    host: process.env.DB_HOST ,
+    user: process.env.DB_USER ,
+    password: process.env.DB_PASSWORD ,
+    database: process.env.DB_NAME ,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+// Transforma o pool para ceitar Promises ( permite usar async/await, deixo código melhor)
+const db = pool.promise();
+
+
+module.exports = db;
+
+
+
+
+
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const db = require('./db');
+
 
 const app = express();
 const PORT = 5000; // O servidor vai rodar nessa porta
@@ -8,9 +10,23 @@ const PORT = 5000; // O servidor vai rodar nessa porta
 app.use(cors());
 app.use(express.json());
 
-// Rota de teste 
-app.get('/api/teste', (req, res) => {
-    res.json({msg: 'Backend conectado com sucesso!'});
+
+
+//Rota GET: busca os produtos do muSQL
+app.get('/api/produtos', async(req, res) => {
+    
+    try {
+        // Executa a query SQL no banco de dados
+        const [linhas] = await db.query('SELECT * FROM produtos');
+
+        res.json(linhas); // returna as linhas encontradas em formato JSON
+
+    } catch (error) {
+        console.log("Erro ao buscar produtos:", error);
+
+        res.status(500).json({error: "Erro interno no servidor ao buscar produtos"});
+    
+    }
 });
 
 // Iniciar o servidor
